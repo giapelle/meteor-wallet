4c41af8b3326da29824bb713dc7dec414b85e8cf
Gianluca Pellegrino <g.pellegrino@azerouno.it>
2022-09-07 11:55:50 +0200

    Bozza Wallet.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import ContactForm from "./ContactForm";
 import ContactList from "./ContactList";
+import Wallet from "./Wallet";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Header />
       <div className="min-h-full">
         <div className="max-w-4xl mx-auto p-2">
+          <Wallet />
           <ContactForm />
           <ContactList />
         </div>
