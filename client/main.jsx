d8ca6a2a5c7a8e39babb8a0e25e8b6b30c9b6450
Gianluca Pellegrino <g.pellegrino@azerouno.it>
2022-12-14 17:23:49 +0100

    Altri test con react-router.

--- client/main.jsx
+++ client/main.jsx
@@ -1,12 +1,28 @@
 import { Meteor } from "meteor/meteor";
+
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "../ui/App";
+import ErrorPage from "../ui/ErrorPage";
 
 import "../api/contacts/method";
 import "../api/transactions/method";
 
+const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 Meteor.startup(() => {
   const root = createRoot(document.getElementById("react-target"));
-  root.render(<App />);
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  );
 });
--- ui/App.jsx
+++ ui/App.jsx
@@ -1,33 +1,29 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import NotFound from "./NotFound";
 import Header from "./Header";
 import Home from "./Home";
 import SignUp from "./SignUp";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={(
-            <div className="h-full">
-              <Header />
-              <div className="min-h-full">
-                <div className="mx-auto max-w-4xl p-2">
-                  <Routes>
-                    <Route index element={<Home />} />
-                    <Route path="signup" element={<SignUp />} />
-                  </Routes>
-                </div>
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <div className="h-full">
+            <Header />
+            <div className="min-h-full">
+              <div className="mx-auto max-w-4xl p-2">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="signup" element={<SignUp />} />
+                </Routes>
               </div>
             </div>
+          </div>
          )}
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+      />
+    </Routes>
   );
 }
