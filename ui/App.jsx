84638732cef8d2468786c26f3af3805f5c862443
Gianluca Pellegrino <g.pellegrino@azerouno.it>
2022-12-14 16:47:34 +0100

    Test (non funzionante) di routing per SignUp.

--- ui/App.jsx
+++ ui/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import NotFound from "./NotFound";
 import Header from "./Header";
 import Home from "./Home";
-import NotFound from "./NotFound";
+import SignUp from "./SignUp";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
                 <div className="mx-auto max-w-4xl p-2">
                   <Routes>
                     <Route index element={<Home />} />
+                    <Route path="signup" element={<SignUp />} />
                   </Routes>
                 </div>
               </div>
